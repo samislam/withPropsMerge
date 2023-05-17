e9ac7f28ac27a6ba489d64c10762503507cfb05f
islam yamor <islamyamor@pm.me>
2023-05-17 12:53:43 +0300

    support for forwardRef

--- @types/withPropsMerge.d.ts
+++ @types/withPropsMerge.d.ts
@@ -1,9 +1,9 @@
-import { FC } from 'react';
 import { Optional } from 'utility-types';
+import React, { FC } from 'react';
 interface MergerProps<ORIGINAL_PROPS extends object> {
     $overwrite?: Optional<ORIGINAL_PROPS>;
     $preserve?: Optional<ORIGINAL_PROPS>;
 }
-declare function withPropsMerge<ORIGINAL_PROPS extends object>(OriginalComponent: FC<ORIGINAL_PROPS>): FC<ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>>;
+declare function withPropsMerge<ORIGINAL_PROPS extends object>(OriginalComponent: FC<ORIGINAL_PROPS>): React.ForwardRefExoticComponent<React.PropsWithoutRef<ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>> & React.RefAttributes<unknown>>;
 export default withPropsMerge;
 //# sourceMappingURL=withPropsMerge.d.ts.map
--- src/withPropsMerge.tsx
+++ src/withPropsMerge.tsx
@@ -1,22 +1,20 @@
 import _ from 'lodash'
-import React, { FC } from 'react'
 import { Optional } from 'utility-types'
 import { mergeObjects } from 'mergerobjects'
+import React, { FC, forwardRef } from 'react'
 
 interface MergerProps<ORIGINAL_PROPS extends object> {
   $overwrite?: Optional<ORIGINAL_PROPS>
   $preserve?: Optional<ORIGINAL_PROPS>
 }
 
-function withPropsMerge<ORIGINAL_PROPS extends object>(
-  OriginalComponent: FC<ORIGINAL_PROPS>
-): FC<ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>> {
-  return (props) => {
+function withPropsMerge<ORIGINAL_PROPS extends object>(OriginalComponent: FC<ORIGINAL_PROPS>) {
+  return forwardRef((props: ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>, ref) => {
     const { $overwrite = {}, $preserve = {} } = props
     const injectedProps = mergeObjects($preserve, props, $overwrite)
     const originalPropsFiltered = _.omit(injectedProps as object, ['$overwrite', '$preserve'])
-    return <OriginalComponent {...(originalPropsFiltered as ORIGINAL_PROPS)} />
-  }
+    return <OriginalComponent {...(originalPropsFiltered as ORIGINAL_PROPS)} ref={ref} />
+  })
 }
 
 export default withPropsMerge
