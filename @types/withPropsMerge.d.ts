b864f254acae1c3bf70cac130659f0ad7f54e287
islam yamor <islamyamor@pm.me>
2023-05-19 00:53:35 +0300

    forgot to build

--- @types/withPropsMerge.d.ts
+++ @types/withPropsMerge.d.ts
@@ -1,8 +1,8 @@
-import { Optional } from 'utility-types';
 import React, { FC } from 'react';
+import { Optional } from 'utility-types';
 interface MergerProps<ORIGINAL_PROPS extends object> {
-    $overwrite?: Optional<ORIGINAL_PROPS>;
-    $preserve?: Optional<ORIGINAL_PROPS>;
+    $overwrite?: Optional<ORIGINAL_PROPS> | Optional<ORIGINAL_PROPS>[];
+    $preserve?: Optional<ORIGINAL_PROPS> | Optional<ORIGINAL_PROPS>[];
 }
 declare function withPropsMerge<ORIGINAL_PROPS extends object>(OriginalComponent: FC<ORIGINAL_PROPS>): React.ForwardRefExoticComponent<React.PropsWithoutRef<ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>> & React.RefAttributes<unknown>>;
 export default withPropsMerge;
