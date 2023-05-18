a34b1c7043dea8484b1cd0186779054c6faad956
islam yamor <islamyamor@pm.me>
2023-05-19 00:13:09 +0300

    added support for multi objects merge using the array syntax

--- src/withPropsMerge.tsx
+++ src/withPropsMerge.tsx
@@ -1,20 +1,23 @@
+import React, { FC, forwardRef } from 'react'
 import _ from 'lodash'
 import { Optional } from 'utility-types'
 import { mergeObjects } from 'mergerobjects'
-import React, { FC, forwardRef } from 'react'
 
 interface MergerProps<ORIGINAL_PROPS extends object> {
-  $overwrite?: Optional<ORIGINAL_PROPS>
-  $preserve?: Optional<ORIGINAL_PROPS>
+  $overwrite?: Optional<ORIGINAL_PROPS> | Optional<ORIGINAL_PROPS>[]
+  $preserve?: Optional<ORIGINAL_PROPS> | Optional<ORIGINAL_PROPS>[]
 }
 
 function withPropsMerge<ORIGINAL_PROPS extends object>(OriginalComponent: FC<ORIGINAL_PROPS>) {
-  return forwardRef((props: ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>, ref) => {
-    const { $overwrite = {}, $preserve = {} } = props
-    const injectedProps = mergeObjects($preserve, props, $overwrite)
-    const originalPropsFiltered = _.omit(injectedProps as object, ['$overwrite', '$preserve'])
-    return <OriginalComponent {...(originalPropsFiltered as ORIGINAL_PROPS)} ref={ref} />
-  })
+  return forwardRef(
+    ({ $overwrite = [], $preserve = [], ...props }: ORIGINAL_PROPS & MergerProps<ORIGINAL_PROPS>, ref) => {
+      const $overwrite_ = _.isArray($overwrite) ? $overwrite : [$overwrite]
+      const $preserve_ = _.isArray($preserve) ? $preserve : [$preserve]
+      const injectedProps = mergeObjects(...$preserve_, props, ...$overwrite_)
+      const originalPropsFiltered = _.omit(injectedProps as any, ['$overwrite', '$preserve'])
+      return <OriginalComponent {...(originalPropsFiltered as ORIGINAL_PROPS)} ref={ref} />
+    }
+  )
 }
 
 export default withPropsMerge
